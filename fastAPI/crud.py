08cfb723bf97caa10ca557a0de1e375b745d0bf0
donghwan <ldh793@naver.com>
2022-12-08 13:59:17 +0900

    Add crud

--- fastAPI/crud.py
+++ fastAPI/crud.py
@@ -1,5 +1,15 @@
-# from sqlalchemy.orm import Session
-# import models, schemas
+from sqlalchemy.orm import Session
+import models, schemas
 
-# def get_celebrities(db: Session, skip: int = 0, limit: int=100):
-#     return db.query(models.Celebrity).offset(skip).limit(limit).all()
+def get_celebrity(db: Session, celebrity_id: int):
+    return db.query(models.Celebrity).filter(models.Celebrity.id == celebrity_id).first()
+
+def get_celebrities(db: Session, skip: int = 0, limit: int=100):
+    return db.query(models.Celebrity).offset(skip).limit(limit).all()
+
+def create_celebrity(db: Session, celebrity: schemas.CelebrityCreate):
+    db_celebrity = models.Celebrity(name=celebrity.name, mbti=celebrity.mbti, image_url=celebrity.image_url)
+    db.add(db_celebrity)
+    db.commit()
+    db.refresh(db_celebrity) # refresh : 자동 생성된 pk까지 포함한 instance로 갱신
+    return db_celebrity
