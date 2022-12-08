abacdf4c87fecd63e17864697ece2a14665cab93
donghwan <ldh793@naver.com>
2022-12-08 13:47:39 +0900

    Add schemas

--- fastAPI/schemas.py
+++ fastAPI/schemas.py
@@ -1,3 +1,17 @@
-# from typing import Union
+from typing import Union
 
-# from pydantic import BaseModel
+from pydantic import BaseModel
+
+class CelebrityBase(BaseModel):
+    name: str
+    mbti: str
+    image_url: str
+
+class CelebrityCreate(CelebrityBase):
+    pass
+
+class Celebrity(CelebrityBase):
+    id: int
+    
+    class Config:
+        orm_mode = True
