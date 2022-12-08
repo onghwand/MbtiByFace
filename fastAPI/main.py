8e44dbd08c3fca1acc4a65a1d4d684c6fa2e8f8a
donghwan <ldh793@naver.com>
2022-12-08 15:00:27 +0900

    Add S3 image_url

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -9,19 +9,36 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
 
-# @app.get("/celebrities/", response_model=list[schemas.Celebrity])
-# def read_celebrities(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     celebrities = crud.get_celebrities(db, skip=skip, limit=limit)
-#     return celebrities
+'''
+연예인 리스트 반환
+'''
+@app.get("/celebrities/", response_model=list[schemas.Celebrity])
+def read_celebrities(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    celebrities = crud.get_celebrities(db, skip=skip, limit=limit)
+    return celebrities
+
+'''
+임의의 사용자 사진을 받아 가장 비슷한 연예인 정보를 반환
+'''
+@app.get("/celebrities/top3", response_model=list[schemas.Celebrity])
+def read_top3(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    celebrities = crud.get_celebrities(db, skip=skip, limit=limit)
+    for celeb in celebrities:
+        print(celeb.image_url, type(celeb))
+    return celebrities
+
+
+
+
 
