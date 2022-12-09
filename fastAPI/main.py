1d365b59dd9f82e1b826fffedbb7bb4ba2cedca1
donghwan <ldh793@naver.com>
2022-12-09 21:06:03 +0900

    Add face_recognition part

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -31,11 +31,31 @@ def read_celebrities(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 '''
 임의의 사용자 사진을 받아 가장 비슷한 연예인 정보를 반환
 '''
+import cv2
+import face_recognition as fr
+import urllib.request as urllib
+from matplotlib import pyplot as plt
 @app.get("/celebrities/top3", response_model=list[schemas.Celebrity])
 def read_top3(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     celebrities = crud.get_celebrities(db, skip=skip, limit=limit)
+    
+    plt.rcParams["figure.figsize"] = (1,1)
+    celeb_images = []
     for celeb in celebrities:
-        print(celeb.image_url, type(celeb))
+        # print(celeb.image_url, type(celeb))
+        response = urllib.urlopen(celeb.image_url)
+        celeb_images.append(fr.load_image_file(response))
+    
+    celeb_faces = [] 
+    for celeb in celeb_images:
+        top, right, bottom, left = fr.face_locations(celeb)[0]
+        face_image = celeb[top:bottom, left:right]
+        
+        celeb_faces.append(face_image)
+    
+    for face in celeb_faces:
+        plt.imshow(face)
+        plt.show()
     return celebrities
 
 
