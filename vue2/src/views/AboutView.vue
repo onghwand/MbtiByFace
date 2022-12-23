f9a8c417a66ba2864842ae62f5fefa1dd0484136
donghwan <ldh793@naver.com>
2022-12-23 21:33:46 +0900

    Add vue pages

--- vue2/src/views/AboutView.vue
+++ vue2/src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Mbti By Face</h1>
+    <p>성격은 <strong>얼굴</strong>에 나타난다</p>
+    <br>
+    <h2>내 사진 넣기</h2>
+    <input type="image">
+    <div>
+      <button>가즈아</button>
+    </div>
   </div>
 </template>
---
+++ vue2/src/views/ResultView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Result</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultView'
+}
+</script>
